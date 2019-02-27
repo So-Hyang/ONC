2d734074ee4a46f64500652d13096e264b7d5220
DESKTOP-05N4PAD\USER <bigshow55555@naver.com>
2019-02-27 22:48:23 +0900

    DBSender 수정 v2

--- DataBaseSender.cpp
+++ DataBaseSender.cpp
@@ -14,7 +14,9 @@ DataBaseSender::~DataBaseSender()
 
 
 
-
+//************DataBaseSender 통신팀 패킷 받아서 DB로 쿼리문 보내주는 함수
+//*****************************************통신 팀에서 쓰삼
+//***************************************02-27 김시완 수정
 
 void DataBaseSender::DB_Data_Send_to_mysql(DataPacket Data) // 함수 수정 02-27 김시완 // 
 {  
@@ -67,8 +69,47 @@ void DataBaseSender::DB_Data_Send_to_mysql(DataPacket Data) // 함수 수정 02-27 �
 
 	}break;
 
-
 	case 2:
+	{
+
+		Contents_Type = "Calender";
+		DB_mysql_connect(DB_CalendarNotice);
+
+		if (Data.PubPrivate == true)
+		{
+			Public_Type = "Public";
+		}
+		if (Data.PubPrivate == false)
+		{
+			Public_Type = "Private";
+		}
+
+		memset(&query, 0, sizeof(query));
+
+		string tmp = "";
+		tmp = "INSERT INTO contents (Date, Who, Contents_Type, Public_Type, Main_Contents) VALUES('";
+		tmp = tmp + Data.cDate;
+		tmp += "', '";
+		tmp += Data.cUserID;
+		tmp += "', '";
+		tmp += Contents_Type;
+		tmp += "', '";
+		tmp += Public_Type;
+		tmp += "', '";
+		tmp += Data.cMsg;
+		tmp += "')";
+		//memcpy(query, tmp.c_str(), strlen(tmp.c_str()));
+		query = (char *)tmp.c_str();
+
+		if (mysql_query(&conn, query))
+		{
+			fprintf(stderr, "%s\n", mysql_error(&conn));
+		}
+		DB_mysql_disconnect(); //02-27 추가 김시완
+
+	}break;
+
+	case 3:
 	{
 		Contents_Type = "Notice";
 		DB_mysql_connect(DB_CalendarNotice);
